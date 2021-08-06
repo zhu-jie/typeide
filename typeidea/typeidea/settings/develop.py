9a9d6e62fa7f4e76d110032f8736a624137b3b52
zhu-jie <792416906@qq.com>
2021-08-06 21:19:39 +0800

    项目初始化阶段

---
+++ typeidea/typeidea/settings/develop.py
@@ -0,0 +1,11 @@
+from .base import * # NOQA
+
+DEBUG = True
+
+# 数据库配置
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
