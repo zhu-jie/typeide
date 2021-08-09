6d8198473b749feca7a305f67342771e1b1451f4
zhu-jie <792416906@qq.com>
2021-08-09 22:40:08 +0800

    增加pip依赖文件

--- typeidea/comment/adminx.py
+++ typeidea/comment/adminx.py
@@ -5,5 +5,5 @@ from typeidea.custom_site import custom_site
 # Register your models here.
 
 @admin.register(Comment, site=custom_site)
-class CommentAdmin(admin.ModelAdmin):
+class CommentAdmin(object):
     list_display = ('target', 'nickname', 'content', 'website', 'email', 'created_time')   
