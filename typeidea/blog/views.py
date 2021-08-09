ec7001a44c35e9ce12c9eb5ea9eb8557c61e9ca2
zhu-jie <792416906@qq.com>
2021-08-09 20:51:37 +0800

    完成访问统计

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,8 +1,10 @@
-from django.db.models import Q
+from datetime import date
+from django.db.models import Q, F
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
 from .models import Post, Tag, Category
 from config.models import SideBar
+from django.core.cache import cache
 # Create your views here.
 
 class CommonViewMixin:
@@ -63,6 +65,30 @@ class PostDetailView(CommonViewMixin, DetailView):
     template_name = 'blog/detail.html'
     context_object_name = 'post'
     pk_url_kwarg = 'post_id'
+
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        self.handle_visited()
+        return response
+    
+    def handle_visited(self):
+        increase_pv = False
+        increase_uv = False
+        uid = self.request.uid
+        pv_key = 'pv:%s:%s' %(uid, self.request.path)
+        uv_key = 'uv:%s:%s:%s' % (uid, str(date.today()), self.request.path)
+        if not cache.get(pv_key):
+            increase_pv = True
+            cache.set(pv_key, 1, 1*60)
+        if not cache.get(uv_key):
+            increase_uv = True
+            cache.set(uv_key, 1, 24*60*60)
+        if increase_pv and increase_uv:
+            Post.objects.filter(pk=self.object.id).update(pv=F('pv') + 1, uv=F('uv') + 1)
+        elif increase_pv:
+            Post.objects.filter(pk=self.object.id).update(pv=F('pv') + 1)
+        elif increase_uv:
+            Post.objects.filter(pk=self.object.id).update(uv=F('uv') + 1)
 
 class SearchView(IndexView):
     def get_context_data(self):
